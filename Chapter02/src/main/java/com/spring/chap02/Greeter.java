2a968b0c4e3cab4b2ed71be7ce665774bcefb10c
taechacode <kangtaechan2015@gmail.com>
2022-04-19 22:58:44 +0900

    Create Chapter02

---
+++ Chapter02/src/main/java/com/spring/chap02/Greeter.java
@@ -0,0 +1,15 @@
+package com.spring.chap02;
+
+public class Greeter {
+	
+	private String format;
+	
+	public String greet(String guest) {
+		return String.format(format, guest);
+	}
+	
+	public void setFormat(String format) {
+		this.format = format;
+	}
+	
+}
